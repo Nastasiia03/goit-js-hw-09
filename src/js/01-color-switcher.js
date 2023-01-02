04188cc9895a2facb29d2b5c843e60dfa21a50a6
Nastasiia03 <abernatskao@gmail.com>
2023-01-02 15:50:25 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
+let timerId = null;
 
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => { setBackgroundColor() }, 1000);
+    startBtn.disabled = true;
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function setBackgroundColor() {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+};
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+});
